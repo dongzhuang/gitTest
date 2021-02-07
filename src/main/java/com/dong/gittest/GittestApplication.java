073941f189829cabd987e8da50c8fbacf113600c
dongzhuang <dongzhuanger@163.com>
2021-02-08 00:40:14 +0800

    第一行

--- src/main/java/com/dong/gittest/GittestApplication.java
+++ src/main/java/com/dong/gittest/GittestApplication.java
@@ -9,6 +9,7 @@ public class GittestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GittestApplication.class, args);
         System.out.println("我是git");
+        System.out.println("第一行");
     }
 
 }
