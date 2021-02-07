8bfe1d3c0bd77806ad45784abf6853f38afff51d
dongzhuang <dongzhuanger@163.com>
2021-02-08 01:25:27 +0800

    时间

--- src/main/java/com/dong/gittest/GittestApplication.java
+++ src/main/java/com/dong/gittest/GittestApplication.java
@@ -3,6 +3,8 @@ package com.dong.gittest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalTime;
+
 @SpringBootApplication
 public class GittestApplication {
 
@@ -11,6 +13,8 @@ public class GittestApplication {
         System.out.println("我是git");
         System.out.println("第一行");
         System.out.println("我是分支");
+        LocalTime now = LocalTime.now();
+        System.out.println(now);
     }
 
 }
